2cf744f40ef264579a0d80b0646e71162d2feafe
Balázs Terényi <balazs.terenyi@gmail.com>
2023-04-02 14:18:29 +0200

    UAT 1.2 red

--- src/cat_and_mouse.js
+++ src/cat_and_mouse.js
@@ -1,5 +1,7 @@
 function catAndMouse(x, j) {
-  return "boring without all three";
+  if (x === "") {
+    return "boring without all three";
+  }  
 }
 
 module.exports = catAndMouse;
--- src/cat_and_mouse.test.js
+++ src/cat_and_mouse.test.js
@@ -4,4 +4,7 @@ describe('US-1', () => {
   it('Given the string x="", jump j=0 When I check if I can catch a mouse Then the result is "boring without all three"', () => {
     expect(catAndMouse("", 0)).toBe("boring without all three");
   });
+  it('Given the string x="...", jump j=0 When I check if I can catch a mouse Then the result is "boring without all three"', () => {
+    expect(catAndMouse("...", 0)).toBe("boring without all three");
+  });
 });
