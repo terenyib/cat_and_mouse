e816622c37f205292a0fff7c4411dffa31ae02e8
Balázs Terényi <balazs.terenyi@gmail.com>
2023-04-02 16:40:24 +0200

    UAT 7.1 green

--- src/cat_and_mouse.js
+++ src/cat_and_mouse.js
@@ -5,15 +5,12 @@ function catAndMouse(x, j) {
   if (cat < 0 || mouse < 0 || dog < 0) {
     return "boring without all three";
   }
-  if ((dog > cat && dog < mouse) || (dog < cat && dog > mouse)) {
+  if (((dog > cat && dog < mouse) && (dog - cat <= j + 1)) || ((dog < cat && dog > mouse) && (cat - dog <= j + 1))) {
     return 'Protected!';
   }
-  if (cat > mouse && (cat - mouse <= j + 1)) {
+  if ((cat > mouse && (cat - mouse <= j + 1)) || (cat < mouse && (mouse - cat <= j + 1))) {
     return "Caught!"
-  }
-  if (cat < mouse && (mouse - cat <= j + 1)) {
-    return "Caught!"
-  }
+  }  
   return "Escaped!";
 }
 
