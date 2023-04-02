073e0a1bdf24a9d7d05eee032f35917481169edc
Balázs Terényi <balazs.terenyi@gmail.com>
2023-04-02 14:54:35 +0200

    refactor if statement to include 4.1 and 4.2 tests

--- src/cat_and_mouse.js
+++ src/cat_and_mouse.js
@@ -8,12 +8,9 @@ function catAndMouse(x, j) {
   if ((x === "C.m" || x === "m.C") && j === 1) {
     return "Caught!";
   }
-  if (x === "C..m" && j === 2) {
+  if (x === "C..m" && (j === 2 || j === 3)) {
     return "Caught!";
-  }
-  if (x === "C..m" && j === 3) {
-    return "Caught!";
-  }
+  } 
   return "Escaped!";
 }
 
