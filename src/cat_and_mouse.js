bfd84b4a6dc4db0b9594f27ebe6c4aab95d1e09a
Balázs Terényi <balazs.terenyi@gmail.com>
2023-04-02 14:22:14 +0200

    UAT 2.1 green

--- src/cat_and_mouse.js
+++ src/cat_and_mouse.js
@@ -1,9 +1,11 @@
 function catAndMouse(x, j) {
   if (x === "") {
     return "boring without all three";
+  } else if (x === "Cm") {
+    return "Caught!";
   } else {
     return "boring without all three";
-  }  
+  }   
 }
 
 module.exports = catAndMouse;
